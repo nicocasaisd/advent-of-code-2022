91e1d143e0e4ffbd445a3b71e04369183b094fd7
nicocasaisd <nicolas.casais.dassie@gmail.com>
2023-03-17 15:57:18 -0300

    added day 6

---
+++ 6day/tunning.py
@@ -0,0 +1,45 @@
+'''
+--- Day 6: Tuning Trouble ---
+'''
+
+FILE_NAME = 'input'
+
+with open(FILE_NAME) as file:
+    datastream = file.readline()
+
+'''
+PART 2
+'''
+
+for i in enumerate(datastream):
+    marker = True
+    if i[0] > 3:
+        buffer = datastream[i[0]-4:i[0]]
+        for item in buffer:
+            if buffer.count(item) > 1:
+                marker = False
+                break
+        
+        if marker:
+            print(i)
+            break
+        
+    
+'''
+PART 2
+'''
+
+
+for i in enumerate(datastream):
+    marker = True
+    if i[0] > 3:
+        buffer = datastream[i[0]-4:i[0]]
+        for item in buffer:
+            if buffer.count(item) > 1:
+                marker = False
+                break
+        
+        if marker:
+            print(i)
+            break
+        
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Advent of Code - Coding Challenge
 
 This is my resolution of the coding challenge calendar from 2022.
-Lots of puzzles that were fun to solve
+Lots of puzzles that were fun to solve. 
+I used Python only
 
 #### 🔗 [Advent of Code 2022](https://adventofcode.com/2022)
